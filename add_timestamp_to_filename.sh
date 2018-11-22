a0992af87849fe4707c04325f4ef57301f0be820
Thomas Ebner <ebner@recog-vision.com>
2018-11-22 10:24:47 +0100

    fixed bash script for files containing spaces

--- add_timestamp_to_filename.sh
+++ add_timestamp_to_filename.sh
@@ -3,14 +3,17 @@
 # current timestamp (e.g. 2018-11-22_09:04:01.353) appended to the filename
 # e.g.: test.png --> test_2018-11-22_09:04:01.353.png
 
-for f in $*
+for f in "$@"
 do
+    echo "$f"
     timestamp=$(stat -c %y "$f")
     timestamp=$(echo $timestamp | sed 's/ +....//g' | sed 's/ /_/g' | sed -E 's/(:..\....)(.*)/\1/g')
 
+    echo "a"
     f_basename=$(basename "$f")
     f_dirname=$(dirname "$f")
-    f_extension=${f_basename##*.}
+    f_extension="${f_basename##*.}"
+
 
     if [ $f_basename = $f_extension ]
     then
@@ -18,12 +21,12 @@ do
         new_filename="${f_dirname}/${f_basename}_$timestamp"
     else
         # filename has extension: e.g. testfile.txt
-        f_basename=$(basename $f ".$f_extension")
+        f_basename=$(basename "$f" ".$f_extension")
         new_filename="${f_dirname}/${f_basename}_$timestamp.${f_extension}"
     fi
     
     # echo $timestamp
-    # echo $new_filename
+    echo $new_filename
         
     cp "$f" "${new_filename}"
 done
